bd2445b09b63f6378738db64e3abf87336dbc0ca
Ali Rzaev <alirzaev1996@gmail.com>
2018-07-16 15:55:32 +0300

    Employ environment variable 'DEBUG' for managing logging level
    If variable is set to '1' then logging level is set to
     'INFO', otherwise level is set to 'ERROR'. If variable
      is undefined then logging level is set to 'ERROR'.

--- misc.py
+++ misc.py
@@ -1,10 +1,17 @@
 import logging
 import datetime
+from os import getenv
+
+
+_DEBUG = getenv('DEBUG', '0')
 
 
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    if _DEBUG == '1':
+        logger.setLevel(logging.INFO)
+    else:
+        logger.setLevel(logging.ERROR)
     handler = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
