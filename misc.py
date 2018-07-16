fd89abb40da040c01410745f432d24a53940e807
Ali Rzaev <alirzaev1996@gmail.com>
2018-07-16 15:57:46 +0300

    Delete function 'get_current_day'
    This function will not be used anymore because current
     day is presented in the response of API
     '/api/v2/schedule'.

--- misc.py
+++ misc.py
@@ -1,5 +1,4 @@
 import logging
-import datetime
 from os import getenv
 
 
@@ -18,21 +17,3 @@ def get_logger(name: str) -> logging.Logger:
     logger.addHandler(handler)
 
     return logger
-
-
-def get_current_day(first_date: str) -> tuple:
-    today = datetime.date.today()
-    d = int(first_date[:2])
-    m = int(first_date[2:4])
-    y = int(first_date[4:])
-    first = datetime.date(y, m, d)
-
-    diff = (today - first).days
-    week = (diff // 7) % 2
-    day = diff % 7
-
-    if day > 5:  # sunday
-        day = (day + 1) % 7
-        week = (week + 1) % 2
-
-    return week, day
